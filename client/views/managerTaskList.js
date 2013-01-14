4dac0f276f57851c09d61aad27314b38b1872f66
Pallavi Anderson <pkaushik@alum.mit.edu>
2013-01-14 13:53:40 -0600

    add basic filtering, separate templates

--- client/views/managerTaskList.js
+++ client/views/managerTaskList.js
@@ -1,19 +1,25 @@
-Template.managerTaskList.filter = function(params) { 
-  Session.set('filter', params); 
-  return this; 
-}
-
-Template.managerTaskList.TASKS = function() {
-  var filter = Session.get('filter');
-  Session.set('filter', null);
-  return filter ? TaskCollection.filter(filter) : TaskCollection.fetch();
-}
-
-Template.managerTaskList.ASSIGNED_NAME = function(id) {
-  return id ? StaffCollection.getNameForId(id) : "Unassigned";
-}
+Template.staffTaskList.helpers({
+  tasks: function() {
+    console.log('called!!' + Session.get("filter"));
+    if (Session.get('filter')) {
+      Session.set('filter', false);
+      var field = Session.get('filter-field');
+      var val = Session.get('filter-value');
+      return Tasks.find({field: val}).fetch();
+    } else {
+      return Tasks.find().fetch();
+    }
+  },
+});
 
 Template.managerTaskList.render = function() {   
   $('#page').html(Meteor.render(Template.managerTaskList));
   return this;
+}
+
+Template.managerTaskList.filter = function(options) { 
+  Session.set('filter-field', options.field); 
+  Session.set('filter-value', options.value); 
+  Session.set('filter', true);
+  return this; 
 }
