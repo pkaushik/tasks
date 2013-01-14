2173b0b12459db40fed486b72e7b1b9365ac6d65
Pallavi Anderson <pkaushik@alum.mit.edu>
2013-01-14 13:54:03 -0600

    separate templates

---
+++ client/views/managerTaskListItem.js
@@ -0,0 +1,8 @@
+Template.managerTaskListItem.helpers({
+  workerName: function(workerId){
+    if (workerId === "unassigned")
+      return "Unassigned";
+    else
+      return Meteor.users.findOne({_id: workerId}).profile.name;
+  }
+});
