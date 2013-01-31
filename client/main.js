27795803d3687fb76af7a1f83fe8b3b5455b7af7
Pallavi Anderson <pkaushik@alum.mit.edu>
2013-01-30 20:56:42 -0600

    initial for mini-pages

--- client/main.js
+++ client/main.js
@@ -1,33 +1,31 @@
-Meteor.startup(function() {
-  // tasks - get all tasks associated
-  Meteor.subscribe("tasks");
-  
-  // users  
-  Meteor.subscribe("directory", function() {
-    // profile is populated after login
-    Router = new Router();
-    Backbone.history.start();
-    
-    // counts 
-    _.each([
-      {status: "R", statusLabel: "label-important", statusName: "Red"}, 
-      {status: "Y", statusLabel: "label-warning", statusName: "Yellow"}, 
-      {status: "G", statusLabel: "label-success", statusName: "Green"}
-    ], function(statusObj) {
-      Meteor.autosubscribe(function () {
-        Meteor.subscribe("counts-by-status", statusObj.statusName, statusObj.statusLabel, statusObj.status);
-      });
-    });
 
-    _.each(Meteor.users.find().fetch(), function(userObj) {
-      Meteor.autosubscribe(function () {
-        Meteor.subscribe("counts-by-staff", userObj.profile.name, userObj._id);
-      });
-      Meteor.subscribe("counts-by-staff", "Unassigned", "unassigned");
+Meteor.subscribe("tasks", function() {
+  _.each([
+    {status: "R", statusLabel: "label-important", statusName: "Red"}, 
+    {status: "Y", statusLabel: "label-warning", statusName: "Yellow"}, 
+    {status: "G", statusLabel: "label-success", statusName: "Green"}
+  ], function(statusObj) {
+    Meteor.autosubscribe(function () {
+      Meteor.subscribe("counts-by-status", statusObj.statusName, statusObj.statusLabel, statusObj.status);
     });
-    
   });
 });
+
+Meteor.subscribe("directory", function() {
+  _.each(Meteor.users.find().fetch(), function(userObj) {
+    Meteor.autosubscribe(function () {
+      Meteor.subscribe("counts-by-staff", userObj.profile.name, userObj._id);
+    });
+    Meteor.subscribe("counts-by-staff", "Unassigned", "unassigned");
+  });
+  
+});
+
+Meteor.pages({
+  '/'                 : { to: 'login' },
+  '/login'            : { to: 'login' }
+});
+
 
 
 Global = {   
